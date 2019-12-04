eb743854edda430962eaec819d3f886f736b3c80
Jordan Evans <jordanevans19@gmail.com>
2019-12-03 23:09:08 -0800

    test

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
+  <?php
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+  ?>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
