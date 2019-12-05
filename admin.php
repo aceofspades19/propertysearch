0e8fb547d62477e2f6fe9351da8501efbfce190e
Jordan Evans <jordanevans19@gmail.com>
2019-12-05 01:51:38 -0800

    removed debugging

--- admin.php
+++ admin.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
   <?php
-    ini_set('display_errors', 1);
+    /* ini_set('display_errors', 1);
     error_reporting(E_ALL);
-    ini_set("display_errors", 1);
+    ini_set("display_errors", 1); */
   ?>
   <head>
     <meta charset="utf-8">
